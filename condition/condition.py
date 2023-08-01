2355fbb76d0ca34c2df258b1b4514aee4229af8f
pengxinyu <xypeng9903@outlook.com>
2023-08-02 00:14:02 +0800

    add xstart_cov_type selection.

--- condition/condition.py
+++ condition/condition.py
@@ -18,6 +18,9 @@ class ConditionOpenAIDenoiser(OpenAIDenoiser):
             operator, 
             measurement, 
             guidance='I',
+            xstart_cov_type='mle',
+            _lambda = None,
+            recon_mse_path = None,
             quantize=False, 
             has_learned_sigmas=True, 
             device='cpu' 
@@ -27,7 +30,16 @@ class ConditionOpenAIDenoiser(OpenAIDenoiser):
         self.operator = operator
         self.y = measurement
         self.guidance = guidance
+        self.xstart_cov_type = xstart_cov_type
+        self._lambda = _lambda
         self.mat_solver = __MAT_SOLVER__[operator.name]
+        self.proximal_solver = __PROXIMAL_SOLVER__[operator.name]
+
+        if recon_mse_path is not None:
+            self.recon_mse = torch.load(recon_mse_path)
+        else:
+            self.recon_mse = None
+            
 
     def forward(self, x: torch.Tensor, sigma):
         if self.guidance == "I":
@@ -35,7 +47,7 @@ class ConditionOpenAIDenoiser(OpenAIDenoiser):
             uncond_xstart_pred = self.uncond_xstart_mean_variance(x, sigma)
             return self._type_I_guidance(
                 uncond_xstart_pred["mean"],
-                uncond_xstart_pred["variance"],
+                uncond_xstart_pred["xstart_cov"],
                 x,
                 sigma
             )
@@ -52,28 +64,46 @@ class ConditionOpenAIDenoiser(OpenAIDenoiser):
 
         xprev_pred = D.p_mean_variance(self.inner_model, x * c_in, t)
         xstart_mean = xprev_pred['pred_xstart']
-        xstart_variance = (
-            (xprev_pred['variance'] - _extract_into_tensor(D.posterior_variance, t, x.shape)) \
-            / _extract_into_tensor(D.posterior_mean_coef1, t, x.shape).pow(2)
-        ).clip(min=0)
-        xstart_variance = xstart_variance if sigma < 0.3 \
-                                          else sigma.pow(2) / (1 + sigma.pow(2)) * torch.ones_like(xstart_variance)
+
+        if self.xstart_cov_type == 'mle':
+            xstart_cov = (
+                (xprev_pred['variance'] - _extract_into_tensor(D.posterior_variance, t, x.shape)) \
+                / _extract_into_tensor(D.posterior_mean_coef1, t, x.shape).pow(2)
+            ).clip(min=0)
+            if self.recon_mse is not None:
+                variance = self.recon_mse[t]
+            else:
+                variance = sigma.pow(2) / (1 + sigma.pow(2))
+            xstart_cov = xstart_cov if sigma < 0.3 \
+                                    else variance * torch.ones_like(xstart_mean)
+        elif self.xstart_cov_type == 'pgdm':
+            xstart_cov = sigma.pow(2) / (1 + sigma.pow(2)) * torch.ones_like(xstart_mean)
+        elif self.xstart_cov_type == 'dps':
+            xstart_cov = 0 * torch.ones_like(xstart_mean)
+        elif self.xstart_cov_type == 'diffpir':
+            assert self._lambda is not None
+            xstart_cov = sigma.pow(2) / self._lambda * torch.ones_like(xstart_mean)
+        elif self.xstart_cov_type == 'analytic':
+            assert self.recon_mse is not None
+            xstart_cov = self.recon_mse[t] * torch.ones_like(xstart_mean)
+        else:
+            raise NotImplementedError
         
         return {
             "mean": xstart_mean,
-            "variance": xstart_variance
+            "xstart_cov": xstart_cov
         }
 
-    def _type_I_guidance(self, xstart_mean, xstart_variance, x, sigma):
+    def _type_I_guidance(self, xstart_mean, xstart_cov, x, sigma):
         assert x.shape[0] == 1
-        mat = self.mat_solver(self.operator, self.y, xstart_mean, xstart_variance)
+        mat = self.mat_solver(self.operator, self.y, xstart_mean, xstart_cov)
         likelihood_score = grad((mat.detach() * xstart_mean).sum(), x)[0]
         xstart_mean = xstart_mean + sigma.pow(2) * likelihood_score
         return xstart_mean.clip(-1, 1).detach()
 
-    def _type_II_guidance(self, hat_x, x, sigma):
-        # TODO
-        raise NotImplementedError
+    def _type_II_guidance(self, xstart_mean, xstart_cov, x, sigma):
+        xstart_mean = self.proximal_solver(self.operator, self.y, xstart_mean, xstart_cov)
+        return xstart_mean.clip(-1, 1).detach()
 
 
 __MAT_SOLVER__ = {}
@@ -87,41 +117,80 @@ def register_mat_solver(name):
 
 
 @register_mat_solver('inpainting')
-def inpainting_mat(operator, y, mean_x0, var_x0):
+def inpainting_mat(operator, y, mean_x0, cov_x0):
     mask = operator.mask
     sigma_s = operator.sigma_s
-    mat =  (mask * y - mask * mean_x0) / (sigma_s.pow(2) + var_x0)
+    mat =  (mask * y - mask * mean_x0) / (sigma_s.pow(2) + cov_x0)
+    return mat
+
+
+def _deblur_mat(operator, y, mean_x0, cov_x0):
+    sigma_s = operator.sigma_s
+    FB, FBC, F2B, FBFy = operator.pre_calculated
+    mat = ifft2((FBFy - F2B * fft2(mean_x0)) / (sigma_s.pow(2) + cov_x0 * F2B)).real
     return mat
 
 
 @register_mat_solver('gaussian_blur')
-def gaussian_blur_mat(operator, y, mean_x0, var_x0):
-    sigma_s = operator.sigma_s
-    FB, FBC, F2B, FBFy = operator.pre_calculated
-    mat = ifft2((FBFy - F2B * fft2(mean_x0)) / (sigma_s.pow(2) + var_x0 * F2B)).real
-    return mat
+def gaussian_blur_mat(operator, y, mean_x0, cov_x0):
+    return _deblur_mat(operator, y, mean_x0, cov_x0)
 
 
 @register_mat_solver('motion_blur')
-def motion_blur_mat(operator, y, mean_x0, var_x0):
-    sigma_s = operator.sigma_s
-    FB, FBC, F2B, FBFy = operator.pre_calculated
-    mat = ifft2((FBFy - F2B * fft2(mean_x0)) / (sigma_s.pow(2) + var_x0 * F2B)).real
-    return mat
+def motion_blur_mat(operator, y, mean_x0, cov_x0):
+    return _deblur_mat(operator, y, mean_x0, cov_x0)
 
 
 @register_mat_solver('super_resolution')
-def super_resolution_mat(operator, y, mean_x0, var_x0):
+def super_resolution_mat(operator, y, mean_x0, cov_x0):
     sigma_s = operator.sigma_s
     sf = operator.scale_factor
     FB, FBC, F2B, FBFy = operator.pre_calculated
-    invW = torch.mean(sr.splits(var_x0 * F2B, sf), dim=-1, keepdim=False)
+    invW = torch.mean(sr.splits(cov_x0 * F2B, sf), dim=-1, keepdim=False)
     mat = ifft2(FBC * (fft2(y - sr.downsample(ifft2(FB * fft2(mean_x0)), sf)) / (sigma_s.pow(2) + invW)).repeat(1, 1, sf, sf)).real
     return mat
 
 
-if __name__ == '__main__':
-    print(__MAT_SOLVER__)
+__PROXIMAL_SOLVER__ = {}
 
+
+def register_proximal_solver(name):
+    def wrapper(func):
+        __PROXIMAL_SOLVER__[name] = func
+        return func
+    return wrapper
+
+
+@register_proximal_solver('inpainting')
+def inpainting_proximal(operator, y, mean_x0, cov_x0):
+    mask = operator.mask
+    sigma_s = operator.sigma_s
+    rho = sigma_s.pow(2) / cov_x0
+    return (mask * y + rho * mean_x0) / (mask + rho)
+
+
+def _deblur_proximal(operator, y, mean_x0, cov_x0):
+    # TODO: untest
+    sigma_s = operator.sigma_s
+    FB, FBC, F2B, FBFy = operator.pre_calculated
+    rho = sigma_s.pow(2) / cov_x0
+    tau = rho.float().repeat(1, 1, 1, 1)
+    return sr.data_solution(mean_x0.float(), FB, FBC, F2B, FBFy, tau, 1)
+
+
+@register_proximal_solver('gaussian_blur')
+def gaussian_blur_proximal(operator, y, mean_x0, cov_x0):
+    return _deblur_proximal(operator, y, mean_x0, cov_x0)
+
+
+@register_proximal_solver('motion_blur')
+def motion_blur_proximal(operator, y, mean_x0, cov_x0):
+    return _deblur_proximal(operator, y, mean_x0, cov_x0)
+
+
+@register_proximal_solver('super_resolution')
+def super_resolution_proximal(operator, y, mean_x0, cov_x0):
+    # TODO
+    pass
 
 
--- sample_condition.py
+++ sample_condition.py
@@ -26,8 +26,6 @@ from torch.utils import data
 from torchvision import transforms
 
 import yaml
-import matplotlib.pyplot as plt
-
 from skimage.metrics import peak_signal_noise_ratio, structural_similarity
 import lpips
 import os
@@ -93,7 +91,8 @@ def main():
     p.add_argument('--steps', type=int, default=50,
                    help='the number of denoising steps')
     p.add_argument('--guidance', type=str, choices=["I", "II"], default="I")
-    p.add_argument('--log-dir', type=str, default=os.path.join("runs", "sample_condition"))
+    p.add_argument('--xstart-cov-type', type=str, choices=["mle", "pgdm", "dps", "diffpir", "analytic"], default="mle")
+    p.add_argument('--logdir', type=str, default=os.path.join("runs", "sample_condition"))
     
     
     #-----------------------------------------
@@ -137,7 +136,7 @@ def main():
 
 
     #---------------------------------------------------
-    # Setup conditional model and do posterior sampling
+    # Setup conditioned model and do posterior sampling
     #---------------------------------------------------
 
     operator_config = load_yaml(args.operator_config)
@@ -146,8 +145,9 @@ def main():
     print(f"Operation: {operator_config['name']} / sigma_s: {operator_config['sigma_s']}")
     
     def run():
-        if not os.path.exists(args.log_dir):
-            os.mkdir(args.log_dir)
+        if not os.path.exists(args.logdir):
+            os.mkdir(args.logdir)
+        save_yaml(vars(args), os.path.join(args.logdir, 'args.yaml'))
 
         loss_fn_vgg = lpips.LPIPS(net='vgg').to(device)
         sigmas = K.sampling.get_sigmas_karras(args.steps, sigma_min, sigma_max, rho=7., device=device)
@@ -163,6 +163,7 @@ def main():
                 operator,
                 measurement,
                 args.guidance,
+                args.xstart_cov_type,
                 device=device
             ).eval()
 
@@ -177,15 +178,15 @@ def main():
             metrics_list.append(metrics)
 
             # qualitative results
-            measurement_filename = os.path.join(args.log_dir, f"{args.prefix}_img_{i}_measurement.png")
+            measurement_filename = os.path.join(args.logdir, f"{args.prefix}_img_{i}_measurement.png")
             K.utils.to_pil_image(measurement).save(measurement_filename)
             for j, out in enumerate(hat_x0):
-                hat_x0_filename = os.path.join(args.log_dir, f"{args.prefix}_img_{i}_hat_x0_sample_{j}.png")
+                hat_x0_filename = os.path.join(args.logdir, f"{args.prefix}_img_{i}_hat_x0_sample_{j}.png")
                 K.utils.to_pil_image(out).save(hat_x0_filename)
 
         avg_metrics = calculate_average_metric(metrics_list)
         print(avg_metrics)
-        save_yaml(avg_metrics, os.path.join(args.log_dir, 'avg_metrics.yaml'))
+        save_yaml(avg_metrics, os.path.join(args.logdir, 'avg_metrics.yaml'))
 
     try:
         run()
