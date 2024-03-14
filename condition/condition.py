2fde8fe731faf573977ba23e3333f512735edb3f
xypeng9903 <xypeng9903@outlook.com>
2024-03-14 11:31:57 +0800

    move CG computation to cpu for saving gpu memory

--- condition/condition.py
+++ condition/condition.py
@@ -337,14 +337,18 @@ def inpainting_mat(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransform()):
         mat =  (mask * y - mask * x0_mean) / (sigma_s.pow(2) + theta0_var) # TODO
     
     else:
+        device = x0_mean.device
+        sigma_s, mask, y, x0_mean, theta0_var = \
+            sigma_s.cpu(), mask.cpu(), y.cpu(), x0_mean.cpu(), theta0_var.cpu()
         ot = ortho_tf
         iot = ortho_tf.inv
+
         class A(LinearOperator):
             def __init__(self):
                 super().__init__(np.float32, (x0_mean.numel(), x0_mean.numel()))
 
             def _matvec(self, mat):
-                mat = torch.Tensor(mat).reshape(x0_mean.shape).to(x0_mean.device)
+                mat = torch.Tensor(mat).reshape(x0_mean.shape)
                 mat = sigma_s**2 * mat + mask * iot(theta0_var * ot(mat))
                 mat = mat.flatten().detach().cpu().numpy()
                 return mat
@@ -353,7 +357,7 @@ def inpainting_mat(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransform()):
         mat, info = cg(A(), b, tol=1e-4, maxiter=1000)
         if info != 0:
             warn('CG not converge.')
-        mat = torch.Tensor(mat).reshape(x0_mean.shape).to(x0_mean.device)
+        mat = torch.Tensor(mat).reshape(x0_mean.shape).to(device)
    
     return mat
 
@@ -367,14 +371,18 @@ def _deblur_mat(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransform()):
         mat = ifft2(FBC / (sigma_s.pow(2) + theta0_var * F2B) * fft2(y - ifft2(FB * fft2(x0_mean)))).real
     
     else:
+        device = x0_mean.device
+        sigma_s, FB, FBC, F2B, FBFy, y, x0_mean, theta0_var = \
+            sigma_s.cpu(), FB.cpu(), FBC.cpu(), F2B.cpu(), FBFy.cpu(), y.cpu(), x0_mean.cpu(), theta0_var.cpu()
         ot = ortho_tf
         iot = ortho_tf.inv
+
         class A(LinearOperator):
             def __init__(self):
                 super().__init__(np.float32, (y.numel(), y.numel()))
 
             def _matvec(self, u):
-                u = torch.Tensor(u).reshape(y.shape).to(y.device)
+                u = torch.Tensor(u).reshape(y.shape)
                 u = sigma_s**2 * u + ifft2(FB * fft2(iot(theta0_var * ot(ifft2(FBC * fft2(u)).real)))).real
                 u = u.flatten().detach().cpu().numpy()
                 return u
@@ -385,8 +393,9 @@ def _deblur_mat(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransform()):
         u, info = cg(A(), b, tol=1e-4, maxiter=1000)
         if info != 0:
             warn('CG not converge.')
-        u = torch.Tensor(u).reshape(y.shape).to(y.device)
-        mat = ifft2(FBC * fft2(u)).real
+        u = torch.Tensor(u).reshape(y.shape)
+
+        mat = (ifft2(FBC * fft2(u)).real).to(device)
    
     return mat
 
@@ -415,14 +424,18 @@ def super_resolution_mat(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransfo
         mat = ifft2(FBC * (fft2(y - sr.downsample(ifft2(FB * fft2(x0_mean)), sf)) / (sigma_s.pow(2) + theta0_var * invW)).repeat(1, 1, sf, sf)).real
     
     else:
+        device = x0_mean.device
+        sigma_s, FB, FBC, F2B, FBFy, y, x0_mean, theta0_var = \
+            sigma_s.cpu(), FB.cpu(), FBC.cpu(), F2B.cpu(), FBFy.cpu(), y.cpu(), x0_mean.cpu(), theta0_var.cpu()
         ot = ortho_tf
         iot = ortho_tf.inv
+
         class A(LinearOperator):
             def __init__(self):
                 super().__init__(np.float32, (y.numel(), y.numel()))
 
             def _matvec(self, u):
-                u = torch.Tensor(u).reshape(y.shape).to(y.device)
+                u = torch.Tensor(u).reshape(y.shape)
                 u = sigma_s**2 * u + sr.downsample(ifft2(FB * fft2(iot(theta0_var * ot(ifft2(FBC * fft2(sr.upsample(u, sf))).real)))), sf)
                 u = u.real.flatten().detach().cpu().numpy()
                 return u
@@ -433,8 +446,9 @@ def super_resolution_mat(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransfo
         u, info = cg(A(), b, tol=1e-4, maxiter=1000)
         if info != 0:
             warn('CG not converge.')
-        u = torch.Tensor(u).reshape(y.shape).to(y.device)
-        mat = ifft2(FBC * fft2(sr.upsample(u, sf))).real
+        u = torch.Tensor(u).reshape(y.shape)
+
+        mat = (ifft2(FBC * fft2(sr.upsample(u, sf))).real).to(device)
 
     return mat
 
@@ -453,40 +467,8 @@ def register_proximal_solver(name):
     return wrapper
 
 
-@register_proximal_solver('colorization')
-def colorization_proximal(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransform()): # TODO
-    sigma_s = operator.sigma_s.clip(min=0.001)
-
-    if theta0_var.numel() == 1:
-        rho = sigma_s.pow(2) / theta0_var
-        d = y.repeat(1, 3, 1, 1) / 3 / rho + x0_mean 
-        cond_x0_mean = d - ((d.mean(dim=1, keepdim=True).repeat(1, 3, 1, 1) / 3) / (1/3 + rho))
-
-    else:
-        ot = ortho_tf
-        iot = ortho_tf.inv
-        class A(LinearOperator):
-            def __init__(self):
-                super().__init__(np.float32, (x0_mean.numel(), x0_mean.numel()))
-
-            def _matvec(self, x):
-                x = torch.Tensor(x).reshape(x0_mean.shape).to(x0_mean.device)
-                x = x.mean(dim=1, keepdim=True).repeat(1, 3, 1, 1) / 3 / sigma_s**2 + iot(ot(x) / theta0_var)
-                x = x.flatten().detach().cpu().numpy()
-                return x
-            
-        b = y.repeat(1, 3, 1, 1) / 3 / sigma_s**2 + iot(ot(x0_mean) / theta0_var)
-        b = b.flatten().detach().cpu().numpy()
-
-        cond_x0_mean, info = cg(A(), b, x0=x0_mean.flatten().detach().cpu().numpy(), tol=1e-4, maxiter=1000)
-        if info != 0:
-            warn('CG not converge.')
-        cond_x0_mean = torch.Tensor(cond_x0_mean).reshape(x0_mean.shape).to(x0_mean.device)
-
-    return cond_x0_mean
-
-
 @register_proximal_solver('inpainting')
+@torch.no_grad()
 def inpainting_proximal(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransform()):
     mask = operator.mask
     sigma_s = operator.sigma_s.clip(min=0.001)
@@ -495,14 +477,18 @@ def inpainting_proximal(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransfor
         cond_x0_mean = (theta0_var * y + sigma_s**2 * x0_mean) / (sigma_s**2 + theta0_var * mask)
     
     else:
+        device = x0_mean.device
+        sigma_s, mask, y, x0_mean, theta0_var = \
+            sigma_s.cpu(), mask.cpu(), y.cpu(), x0_mean.cpu(), theta0_var.cpu()
         ot = ortho_tf
         iot = ortho_tf.inv
+
         class A(LinearOperator):
             def __init__(self):
                 super().__init__(np.float32, (x0_mean.numel(), x0_mean.numel()))
 
             def _matvec(self, x):
-                x = torch.Tensor(x).reshape(x0_mean.shape).to(x0_mean.device)
+                x = torch.Tensor(x).reshape(x0_mean.shape)
                 x = mask * x / sigma_s**2 + iot(ot(x) / theta0_var)
                 x = x.flatten().detach().cpu().numpy()
                 return x
@@ -513,11 +499,12 @@ def inpainting_proximal(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransfor
         cond_x0_mean, info = cg(A(), b, x0=x0_mean.flatten().detach().cpu().numpy(), tol=1e-4, maxiter=1000)
         if info != 0:
             warn('CG not converge.')
-        cond_x0_mean = torch.Tensor(cond_x0_mean).reshape(x0_mean.shape).to(x0_mean.device) 
+        cond_x0_mean = torch.Tensor(cond_x0_mean).reshape(x0_mean.shape).to(device) 
     
     return cond_x0_mean
 
 
+@torch.no_grad()
 def _deblur_proximal(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransform()):
     sigma_s = operator.sigma_s.clip(min=0.001)
     FB, FBC, F2B, FBFy = operator.pre_calculated
@@ -528,14 +515,18 @@ def _deblur_proximal(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransform()
         cond_x0_mean = sr.data_solution(x0_mean.float(), FB, FBC, F2B, FBFy, tau, 1).float()
     
     else:
+        device = x0_mean.device
+        sigma_s, FB, FBC, F2B, FBFy, x0_mean, theta0_var = \
+            sigma_s.cpu(), FB.cpu(), FBC.cpu(), F2B.cpu(), FBFy.cpu(), x0_mean.cpu(), theta0_var.cpu()
         ot = ortho_tf
         iot = ortho_tf.inv
+
         class A(LinearOperator):
             def __init__(self):
                 super().__init__(np.float32, (x0_mean.numel(), x0_mean.numel()))
 
             def _matvec(self, x):
-                x = torch.Tensor(x).reshape(x0_mean.shape).to(x0_mean.device)
+                x = torch.Tensor(x).reshape(x0_mean.shape)
                 x = ifft2(F2B * fft2(x)).real / sigma_s**2 + iot(ot(x) / theta0_var)
                 x = x.flatten().detach().cpu().numpy()
                 return x
@@ -546,22 +537,25 @@ def _deblur_proximal(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransform()
         cond_x0_mean, info = cg(A(), b, x0=x0_mean.flatten().detach().cpu().numpy(), tol=1e-4, maxiter=1000)
         if info != 0:
             warn('CG not converge.')
-        cond_x0_mean = torch.Tensor(cond_x0_mean).reshape(x0_mean.shape).to(x0_mean.device) 
+        cond_x0_mean = torch.Tensor(cond_x0_mean).reshape(x0_mean.shape).to(device) 
     
     return cond_x0_mean
 
 
 @register_proximal_solver('gaussian_blur')
+@torch.no_grad()
 def gaussian_blur_proximal(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransform()):
     return _deblur_proximal(operator, y, x0_mean, theta0_var, ortho_tf)
 
 
 @register_proximal_solver('motion_blur')
+@torch.no_grad()
 def motion_blur_proximal(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransform()):
     return _deblur_proximal(operator, y, x0_mean, theta0_var, ortho_tf)
 
 
 @register_proximal_solver('super_resolution')
+@torch.no_grad()
 def super_resolution_proximal(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTransform()):
     sigma_s = operator.sigma_s.clip(min=0.001)
     sf = operator.scale_factor
@@ -573,14 +567,18 @@ def super_resolution_proximal(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTr
         cond_x0_mean = sr.data_solution(x0_mean.float(), FB, FBC, F2B, FBFy, tau, sf).float()
 
     else:
+        device = x0_mean.device
+        sigma_s, FB, FBC, F2B, FBFy, x0_mean, theta0_var = \
+            sigma_s.cpu(), FB.cpu(), FBC.cpu(), F2B.cpu(), FBFy.cpu(), x0_mean.cpu(), theta0_var.cpu()
         ot = ortho_tf
         iot = ortho_tf.inv
+
         class A(LinearOperator):
             def __init__(self):
                 super().__init__(np.float32, (x0_mean.numel(), x0_mean.numel()))
 
             def _matvec(self, x):
-                x = torch.Tensor(x).reshape(x0_mean.shape).to(x0_mean.device)
+                x = torch.Tensor(x).reshape(x0_mean.shape)
                 x = ifft2(FBC * fft2(sr.upsample(sr.downsample(ifft2(FB * fft2(x)), sf), sf))).real / sigma_s**2 + iot(ot(x) / theta0_var)
                 x = x.flatten().detach().cpu().numpy()
                 return x
@@ -591,6 +589,6 @@ def super_resolution_proximal(operator, y, x0_mean, theta0_var, ortho_tf=OrthoTr
         cond_x0_mean, info = cg(A(), b, x0=x0_mean.flatten().detach().cpu().numpy(), tol=1e-4, maxiter=1000)
         if info != 0:
             warn('CG not converge.')
-        cond_x0_mean = torch.Tensor(cond_x0_mean).reshape(x0_mean.shape).to(x0_mean.device)
+        cond_x0_mean = torch.Tensor(cond_x0_mean).reshape(x0_mean.shape).to(device)
 
     return cond_x0_mean
