da28bb28acce0d05965e3ad611508add26731663
pengxinyu <xypeng9903@outlook.com>
2024-03-29 20:26:13 +0800

    update eval_guidance_stsl.sh

--- quick_start/eval_guidance_stsl.sh
+++ quick_start/eval_guidance_stsl.sh
@@ -1,4 +1,5 @@
 DATASET="$1"
+ETA="$2"
 
 if [ "${DATASET}" = "ffhq" ]; then
     CONFIG="configs/test_ffhq.json"
@@ -10,53 +11,51 @@ else
     echo "Invalid dataset."
 fi
 
-for GAMMA in 0 1e-2 1e-1 1
-do
-    python sample_condition_openai.py \
-    --guidance stsl \
-    --save-img \
-    --config ${CONFIG} \
-    --checkpoint ${CHECKPOINT} \
-    --operator-config configs/gaussian_deblur_config.yaml \
-    --logdir runs/sample_condition_openai/guidance_stsl/${DATASET}/gaussian_deblur/${COV}/zeta_1e3_gamma_${GAMMA} \
-    --zeta 1e3 \
-    --gamma ${GAMMA} \
-    --num-hutchinson-samples 1 \
-    --ode
 
-    # python sample_condition_openai.py \
-    # --guidance stsl \
-    # --save-img \
-    # --config ${CONFIG} \
-    # --checkpoint ${CHECKPOINT} \
-    # --operator-config configs/motion_deblur_config.yaml \
-    # --logdir runs/sample_condition_openai/guidance_stsl/${DATASET}/motion_deblur/${COV}/zeta_1e2_gamma_${GAMMA} \
-    # --zeta 1e2 \
-    # --num-hutchinson-samples 1 \
-    # --gamma ${GAMMA} \
-    # --ode
+python sample_condition_openai.py \
+--guidance stsl \
+--save-img \
+--config ${CONFIG} \
+--checkpoint ${CHECKPOINT} \
+--operator-config configs/gaussian_deblur_config.yaml \
+--logdir runs/sample_condition_openai/guidance_stsl/${DATASET}/gaussian_deblur/${COV}/zeta_1e3_eta_${ETA} \
+--zeta 1e3 \
+--eta ${ETA} \
+--num-hutchinson-samples 1 \
+--ode
 
-    # python sample_condition_openai.py \
-    # --guidance stsl \
-    # --save-img \
-    # --config ${CONFIG} \
-    # --checkpoint ${CHECKPOINT} \
-    # --operator-config configs/inpainting_config.yaml \
-    # --logdir runs/sample_condition_openai/guidance_stsl/${DATASET}/inpaint/${COV}/zeta_1e3_gamma_${GAMMA} \
-    # --zeta 1e3 \
-    # --gamma ${GAMMA} \
-    # --num-hutchinson-samples 1 \
-    # --ode
+python sample_condition_openai.py \
+--guidance stsl \
+--save-img \
+--config ${CONFIG} \
+--checkpoint ${CHECKPOINT} \
+--operator-config configs/motion_deblur_config.yaml \
+--logdir runs/sample_condition_openai/guidance_stsl/${DATASET}/motion_deblur/${COV}/zeta_1e2_eta_${ETA} \
+--zeta 1e2 \
+--num-hutchinson-samples 1 \
+--eta ${ETA} \
+--ode
 
-    # python sample_condition_openai.py \
-    # --guidance stsl \
-    # --save-img \
-    # --config ${CONFIG} \
-    # --checkpoint ${CHECKPOINT} \
-    # --operator-config configs/super_resolution_4x_config.yaml \
-    # --logdir runs/sample_condition_openai/guidance_stsl/${DATASET}/super_resolution/${COV}/zeta_1e3_gamma_${GAMMA} \
-    # --zeta 1e3 \
-    # --gamma ${GAMMA} \
-    # --num-hutchinson-samples 1 \
-    # --ode
-done
+python sample_condition_openai.py \
+--guidance stsl \
+--save-img \
+--config ${CONFIG} \
+--checkpoint ${CHECKPOINT} \
+--operator-config configs/inpainting_config.yaml \
+--logdir runs/sample_condition_openai/guidance_stsl/${DATASET}/inpaint/${COV}/zeta_1e3_eta_${ETA} \
+--zeta 1e3 \
+--eta ${ETA} \
+--num-hutchinson-samples 1 \
+--ode
+
+python sample_condition_openai.py \
+--guidance stsl \
+--save-img \
+--config ${CONFIG} \
+--checkpoint ${CHECKPOINT} \
+--operator-config configs/super_resolution_4x_config.yaml \
+--logdir runs/sample_condition_openai/guidance_stsl/${DATASET}/super_resolution/${COV}/zeta_1e3_eta_${ETA} \
+--zeta 1e3 \
+--eta ${ETA} \
+--num-hutchinson-samples 1 \
+--ode
